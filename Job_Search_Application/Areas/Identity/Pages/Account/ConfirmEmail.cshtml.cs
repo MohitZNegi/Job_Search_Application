6b54dd9dd644a30de1472d70ef6e5f67618cb962
MohitZNegi <mohit26negi@gmail.com>
2023-05-15 15:17:40 +1200

    Revert "Email Verification Change"
    This reverts commit d8c21d7a7dbd749861475eafe47d46cb4b79f872.

--- Job_Search_Application/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Job_Search_Application/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -3,112 +3,50 @@
 #nullable disable
 
 using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Job_Search_Application.Data;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Options;
-using SendGrid.Helpers.Mail;
-using SendGrid;
-using Microsoft.DotNet.Scaffolding.Shared.Messaging;
-using Microsoft.Extensions.Logging;
 
 namespace Job_Search_Application.Areas.Identity.Pages.Account
 {
-    public class SendGridEmailSender : IEmailSender
+    public class ConfirmEmailModel : PageModel
     {
-        private readonly IConfiguration configuration;
-        private readonly ILogger logger;
+        private readonly UserManager<ApplicationUsers> _userManager;
 
-        public SendGridEmailSender(IConfiguration configuration, ILogger<SendGridEmailSender> logger)
+        public ConfirmEmailModel(UserManager<ApplicationUsers> userManager)
         {
-            this.configuration = configuration;
-            this.logger = logger;
+            _userManager = userManager;
         }
 
-        public AuthMessageSenderOptions Options { get; } //Set with Secret Manager.
-
-        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+        public async Task<IActionResult> OnGetAsync(string userId, string code)
         {
-            string sendGridApiKey = configuration["SG.lRKpFCYdS5-kYcFnE_Tkhw.nAxWOgDtuWyeAbR3iKSRtwOrTdlMN1nz5RnY7_nIXj8y"];
-            if (string.IsNullOrEmpty(sendGridApiKey))
+            if (userId == null || code == null)
             {
-                throw new Exception("The 'SendGridApiKey' is not configured");
+                return RedirectToPage("/Index");
             }
 
-            var client = new SendGridClient(sendGridApiKey);
-            var msg = new SendGridMessage()
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
             {
-                From = new EmailAddress("aakesh.sharma@techtorium.ac.nz", "WorkWize"),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
-            };
-            msg.AddTo(new EmailAddress(toEmail));
-
-            var response = await client.SendEmailAsync(msg);
-            if (response.IsSuccessStatusCode)
-            {
-                logger.LogInformation("Email queued successfully");
+                return NotFound($"Unable to load user with ID '{userId}'.");
             }
-            else
-            {
-                logger.LogError("Failed to send email");
-                // Adding more information related to the failed email could be helpful in debugging failure,
-                // but be careful about logging PII, as it increases the chance of leaking PII
-            }
-        }
 
-        public async Task Execute(string apiKey, string subject, string message, string toEmail)
-        {
-            var client = new SendGridClient(apiKey);
-            var msg = new SendGridMessage()
-            {
-                From = new EmailAddress("Joe@contoso.com", "Password Recovery"),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
-            };
-            msg.AddTo(new EmailAddress(toEmail));
-
-            // Disable click tracking.
-            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
-            msg.SetClickTracking(false, false);
-            var response = await client.SendEmailAsync(msg);
-            logger.LogInformation(response.IsSuccessStatusCode
-                                   ? $"Email to {toEmail} queued successfully!"
-                                   : $"Failure Email to {toEmail}");
-        }
-        private static void Main()
-        {
-            Execute().Wait();
-        }
-
-        static async Task Execute()
-        {
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("aakesh.sharma@techtorium.ac.nz", "Aakesh");
-            var subject = "Please confirm your email address";
-            var to = new EmailAddress("aakesh.sharma@techtorium.ac.nz", "Test");
-            var plainTextContent = "Please Confirm your email Address";
-            var htmlContent = "<strong>Please Confirm your email Address</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-            Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Body.ReadAsStringAsync());
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            return Page();
         }
     }
-
-
-    public class AuthMessageSenderOptions
-    {
-        internal string SendGridKey;
-    }
-
 }
