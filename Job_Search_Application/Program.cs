91b4993829e14a1a322f43aad9497e84a4001220
MohitZNegi <mohit26negi@gmail.com>
2023-05-17 16:27:50 +1200

    Revert "Merge branch 'User_Profile_Setup' into User_Profile_UI"
    This reverts commit 068e858b4b64220d15dcabb6110c4d025b5c2e85, reversing
    changes made to 26aa4f238edd7ed88de5cb7af343a81c9cbd0186.

--- Job_Search_Application/Program.cs
+++ Job_Search_Application/Program.cs
@@ -1,23 +1,14 @@
 using Job_Search_Application.Data;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Job_Search_Application.Services;
-using WebPWrecover.Services;
-using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-
-
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));                    
+    options.UseSqlServer(connectionString));
 
 //builder.Services.AddDefaultIdentity<ApplicationUsers>(options => options.SignIn.RequireConfirmedAccount = true)
   //  .AddEntityFrameworkStores<ApplicationDbContext>();
@@ -27,8 +18,7 @@ builder.Services.AddIdentity<ApplicationUsers, IdentityRole>().AddDefaultTokenPr
     .AddDefaultUI()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -55,7 +45,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-
 
 using (var scope = app.Services.CreateScope())
 {
