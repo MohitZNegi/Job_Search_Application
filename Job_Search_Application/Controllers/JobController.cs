a8fe140dfae93c4d263a2087f4c737b4ffe4b56d
MohitZNegi <mohit26negi@gmail.com>
2023-05-31 11:07:51 +1200

    Query/Filters Added to the jobs feed

--- Job_Search_Application/Controllers/JobController.cs
+++ Job_Search_Application/Controllers/JobController.cs
@@ -32,30 +32,80 @@ namespace Job_Search_Application.Controllers
         {
             return View();
         }
-
-        public ActionResult JobsFeed(string searchTerm, int? page)
+        public ActionResult JobsFeed(string searchTerm1, string searchTerm2, string searchTerm3, string searchTerm4, string searchTerm5, string searchTerm6, int? page)
         {
             var userId = _userManager.GetUserId(HttpContext.User);
             ViewBag.CurrentUser = userId;
             var jobs = _context.Jobs.Include(j => j.Employer).ToList();
 
-
-
-            if (!String.IsNullOrWhiteSpace(searchTerm))
+            if (!string.IsNullOrWhiteSpace(searchTerm1))
             {
                 jobs = _context.Jobs
-                                    .Where(j => j.Title.Contains(searchTerm) || j.Job_Location.Contains(searchTerm)
-                                            || j.Job_Type.Contains(searchTerm)
-                                            || j.Job_Schedule.Contains(searchTerm)
-                                            || j.Classification.Contains(searchTerm)
-                                            || j.Employer.Company_Name.Contains(searchTerm))
-                                    .Include(j => j.Employer)
-                                    .ToList();
-
+                      .Where(j => j.Title.Contains(searchTerm1)
+                              || j.Job_Type.Contains(searchTerm1)
+                              || j.Job_Schedule.Contains(searchTerm1)
+                              || j.Classification.Contains(searchTerm1)
+                              || j.Job_Details.Contains(searchTerm1)
+                              || j.Employer.Company_Name.Contains(searchTerm1))
+                      .Include(j => j.Employer)
+                      .ToList();
             }
+            else if (!string.IsNullOrWhiteSpace(searchTerm2))
+            {
+                // Search by location
+                jobs = jobs.Where(j => j.Job_Location.Contains(searchTerm2)).ToList();
+            }
+
+            if (!String.IsNullOrWhiteSpace(searchTerm3))
+            {
+                jobs = jobs.Where(j => j.Job_Type.Contains(searchTerm3)).ToList();
+            }
+
+            if (!String.IsNullOrWhiteSpace(searchTerm4))
+            {
+                jobs = jobs.Where(j => j.Job_Schedule.Contains(searchTerm4)).ToList();
+            }
+
+            if (!String.IsNullOrWhiteSpace(searchTerm5))
+            {
+                jobs = jobs.Where(j => j.Classification.Contains(searchTerm5)).ToList();
+            }
+
+            if (!String.IsNullOrWhiteSpace(searchTerm6))
+            {
+                // Parse the search term to numeric values
+                var searchMinSalary = 0;
+                var searchMaxSalary = int.MaxValue;
+
+                if (searchTerm6 == "Below $50,000")
+                {
+                    searchMaxSalary = 50000;
+                }
+                else if (searchTerm6 == "$50,000 - $100,000")
+                {
+                    searchMinSalary = 50000;
+                    searchMaxSalary = 100000;
+                }
+                else if (searchTerm6 == "$100,000 - $150,000")
+                {
+                    searchMinSalary = 100000;
+                    searchMaxSalary = 150000;
+                }
+                // Add more salary ranges as needed
+
+                // Filter jobs within the salary range
+                jobs = jobs.Where(j =>
+                    !String.IsNullOrWhiteSpace(j.Salary) &&
+                    int.TryParse(j.Salary, out var jobSalary) &&
+                    jobSalary >= searchMinSalary &&
+                    jobSalary <= searchMaxSalary
+                ).ToList();
+            }
+
 
             return View(jobs.ToPagedList(page ?? 1, 3));
         }
+
 
         public ActionResult JobsApplication(string id)
         {
